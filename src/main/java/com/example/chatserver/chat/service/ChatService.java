eb89e9105820cad84ff7edca6f76a22e142c6622
YoungKwanK <young.kwin@gmail.com>
2025-08-05 22:06:43 +0900

    메시지저장

--- src/main/java/com/example/chatserver/chat/service/ChatService.java
+++ src/main/java/com/example/chatserver/chat/service/ChatService.java
@@ -1,12 +1,21 @@
 package com.example.chatserver.chat.service;
 
+import com.example.chatserver.chat.domain.ChatMessage;
+import com.example.chatserver.chat.domain.ChatParticipant;
+import com.example.chatserver.chat.domain.ChatRoom;
+import com.example.chatserver.chat.domain.ReadStatus;
+import com.example.chatserver.chat.dto.ChatMessageReqDto;
 import com.example.chatserver.chat.repository.ChatMessageRepository;
 import com.example.chatserver.chat.repository.ChatParticipantRepository;
 import com.example.chatserver.chat.repository.ChatRoomRepository;
 import com.example.chatserver.chat.repository.ReadStatusRepository;
+import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -23,5 +32,33 @@ public class ChatService {
         this.chatMessageRepository = chatMessageRepository;
         this.readStatusRepository = readStatusRepository;
         this.memberRepository = memberRepository;
+    }
+    
+    public void saveMessage(Long roomId, ChatMessageReqDto chatMessageReqDto) {
+//        채팅방 조회
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(()->new EntityNotFoundException("room cannot find"));
+
+//        보낸 사람 조회
+        Member sender = memberRepository.findByEmail(chatMessageReqDto.getSenderEmail()).orElseThrow(()->new EntityNotFoundException("room cannot find"));
+
+//        메시지 저장
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(chatRoom)
+                .member(sender)
+                .content(chatMessageReqDto.getMessage())
+                .build();
+        chatMessageRepository.save(chatMessage);
+        
+//        사용자별로 읽음여부 저장
+        List<ChatParticipant> chatParticipants = chatParticipantRepository.findByChatRoom(chatRoom);
+        for(ChatParticipant chatParticipant : chatParticipants) {
+            ReadStatus readStatus = ReadStatus.builder()
+                    .chatRoom(chatRoom)
+                    .member(chatParticipant.getMember())
+                    .chatMessage(chatMessage)
+                    .isRead(chatParticipant.getMember().equals(sender))
+                    .build();
+            readStatusRepository.save(readStatus);
+        }
     }
 }
