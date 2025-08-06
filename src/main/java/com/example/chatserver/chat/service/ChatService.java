6e51736900500881dcb5663463141aa4b614b5b2
YoungKwanK <young.kwin@gmail.com>
2025-08-06 15:23:25 +0900

    채팅방 생성

--- src/main/java/com/example/chatserver/chat/service/ChatService.java
+++ src/main/java/com/example/chatserver/chat/service/ChatService.java
@@ -12,6 +12,7 @@ import com.example.chatserver.chat.repository.ReadStatusRepository;
 import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,5 +61,23 @@ public class ChatService {
                     .build();
             readStatusRepository.save(readStatus);
         }
+    }
+
+    public void createGroupRoom(String chatRoomName){
+        Member member = memberRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName()).orElseThrow(()->new EntityNotFoundException("member not found"));
+        
+//        채팅방 생성
+        ChatRoom chatRoom = ChatRoom.builder()
+                .name(chatRoomName)
+                .isGroupChat("Y")
+                .build();
+        chatRoomRepository.save(chatRoom);
+
+//        채팅참여자로 개설자를 추가
+        ChatParticipant chatParticipant = ChatParticipant.builder()
+                .chatRoom(chatRoom)
+                .member(member)
+                .build();
+        chatParticipantRepository.save(chatParticipant);
     }
 }
